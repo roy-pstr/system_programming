874aeafbdc73ef1f72fe009d935f47bdcca89a1c
ilayamar <57702448+ilayamar@users.noreply.github.com>
2019-11-13 11:12:05 +0200

    FILE WRITING

--- HW1/Father/main.c
+++ HW1/Father/main.c
@@ -9,18 +9,22 @@ void runParsing(char *math_expression) {
 	int result = 0, next_close_brks_ind = 0, output_ind = 0;
 	char* next_close_brkt_ptr;
 	char simple_math_exp[SIMPLE_MATH_STRING_MAX_LEN];
-
+	FILE *fp;
+	fp = fopen("Computation.txt", "w"); //ILAY FILE EXCEPTIONS
 	next_close_brkt_ptr = strchr(math_expression, ')');
+	fprintf(fp, "%s\n", math_expression);
 	while (next_close_brkt_ptr != NULL) {
 		output_ind++;
 		next_close_brks_ind = (int)(next_close_brkt_ptr - math_expression);
 		extractSimpleMathExpression(simple_math_exp, math_expression, next_close_brks_ind);
 		result = calcResultUsingSon(simple_math_exp); //using other process...
 		updateMathExpWithResult(math_expression, simple_math_exp, result);
-		printf("%s\n", math_expression);
+		fprintf(fp, "%s\n", math_expression);
 
 		next_close_brkt_ptr = strchr(math_expression, ')');
 	}
+	fclose(fp);
+
 }
 
 int main(int argc, char *argv[])
