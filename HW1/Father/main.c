7ce80124ac89b4c4295ebbc208fd1e0cae3fd38f
roy-pstr <49519972+roy-pstr@users.noreply.github.com>
2019-11-13 10:54:31 +0200

    working version...

--- HW1/Father/main.c
+++ HW1/Father/main.c
@@ -5,55 +5,35 @@
 #include "process_handler.h"
 #include "math_expression_parser.h"
 
-void runParsing(char *math_expression, char **parser_outputs) {
+void runParsing(char *math_expression) {
 	int result = 0, next_close_brks_ind = 0, output_ind = 0;
 	char* next_close_brkt_ptr;
 	char simple_math_exp[SIMPLE_MATH_STRING_MAX_LEN];
 
 	next_close_brkt_ptr = strchr(math_expression, ')');
-	writeStringToOutputsArray(parser_outputs, output_ind, math_expression);
-
-	while (next_close_brkt_ptr!=NULL) {
+	while (next_close_brkt_ptr != NULL) {
 		output_ind++;
 		next_close_brks_ind = (int)(next_close_brkt_ptr - math_expression);
 		extractSimpleMathExpression(simple_math_exp, math_expression, next_close_brks_ind);
 		result = calcResultUsingSon(simple_math_exp); //using other process...
 		updateMathExpWithResult(math_expression, simple_math_exp, result);
-		//writeStringToOutputsArray(parser_outputs, math_expression);
 		printf("%s\n", math_expression);
 
 		next_close_brkt_ptr = strchr(math_expression, ')');
 	}
 }
- 
-void releaseOutputsMemory(char **parser_outputs) {
-
-}
 
 int main(int argc, char *argv[])
 {
 	int result = 0;
-	char **parser_outputs;
-
 	if (argc != 2)
 	{
 		printf("Illegal number of arguents! Try again\n");
 		return ERROR_CODE;
 	}
 
-	parser_outputs = malloc(1*sizeof(char*));
-	if (parser_outputs) {
-		parser_outputs[0] = malloc(MATH_STRING_MAX_LEN);
-	}
-	else {
-		printf("memory allocation have been failed.\n");
-		return ERROR_CODE;
-	}
-
 	printf("%s\n", argv[1]); //debug
 
-	runParsing(argv[1], parser_outputs);
-	writeParsingsToFile(parser_outputs);
-	releaseOutputsMemory(parser_outputs);
+	runParsing(argv[1]);
 	return SUCCESS_CODE;
 }
