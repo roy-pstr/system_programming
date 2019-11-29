6a9de6f62b5d9f62261e29de7bf7e4cfb63dfff2
ilayamar <57702448+ilayamar@users.noreply.github.com>
2019-11-29 17:57:52 +0200

    started working on Testmanager

--- HW2/TestManager/main.c
+++ HW2/TestManager/main.c
@@ -1,3 +1,19 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(int argc, char *argv[]) 
+{
+	// NEED TO CREATE A FUNCTION THAT CREATES THE CMD LINE AND SENDS IT TO A PROCESS (TestGrades.exe)
+
+	//need to do argv[1] as grade directory and argv[1] + grades_id to send to process.
+
+		//check argv:
+	if (argc != 2)
+	{
+		printf("Illegal number of arguents! Try again\n");
+		return -1;  // DEBUG ERROR_CODE
+	}
+
+
 	return 0;
 }
