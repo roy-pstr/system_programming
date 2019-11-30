bfbbe282a74bc64a2a8b45d5b62b99b6265d7b8d
ilayamar <57702448+ilayamar@users.noreply.github.com>
2019-11-30 15:14:43 +0200

    process make

--- HW2/TestManager/main.c
+++ HW2/TestManager/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include "manager_handler.h"
 int main(int argc, char *argv[]) 
 {
 	// NEED TO CREATE A FUNCTION THAT CREATES THE CMD LINE AND SENDS IT TO A PROCESS (TestGrades.exe)
@@ -13,7 +12,10 @@ int main(int argc, char *argv[])
 		printf("Illegal number of arguents! Try again\n");
 		return -1;  // DEBUG ERROR_CODE
 	}
+	printf("ARG1 = %s\n", argv[1]);
+	MultipleProcessesCalling(argv[1]);
 
+	printf("DONE");
 
 	return 0;
 }
