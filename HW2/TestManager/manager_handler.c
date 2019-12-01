81ef1d4ca74ef7b016233859c535c0ad5cfef530
roy-pstr <49519972+roy-pstr@users.noreply.github.com>
2019-12-01 21:35:07 +0200

    changed +r to a, impl. the 'allocateStr' function in manager_handler.c

--- HW2/TestManager/manager_handler.c
+++ HW2/TestManager/manager_handler.c
@@ -4,30 +4,48 @@
 #include <windows.h>
 #include "manager_handler.h"
 #include "process_handler.h"
+void allocateString(char **str_ptr, int len) {
+	if (NULL == (*str_ptr = (char *)malloc(len)))
+	{
+		printf("Memory Allocation failed! Try again...\n");
+		exit(ERROR_CODE);
+	}
+}
 
 int MultipleProcessesCalling(char *directory)
 {
 	char id_number[ID_LENGTH];
 	char *id_filepath, *final_filepath, *student_dir;
+
+	/* allocate memory for strings */
+	allocateString(&id_filepath, (strlen(directory) + strlen(ID_FILE_NAME) + 2)); //malloc, must free!
+	allocateString(&final_filepath, (strlen(directory) + strlen(FINAL_GRADES_FILE_NAME) + 2)); //malloc, must free!
+	allocateString(&student_dir, (strlen(directory) + ID_LENGTH + strlen(DIRECTORY_FOR_SON) + 2)); //malloc, must free!
 	// DEBUG DO FUNCTION FOR ALL MALLOCS
-	if (NULL == (id_filepath = (char *)malloc(strlen(directory) + strlen(ID_FILE_NAME) + 2)))  // student Ids memory allocation
-	{
-		printf("Memory Allocation failed! Try again...\n");
-		exit(ERROR_CODE); 
-	}
-	if (NULL == (final_filepath = (char *)malloc(strlen(directory) + strlen(FINAL_GRADES_FILE_NAME) + 2)))  // student Ids memory allocation
-	{
-		printf("Memory Allocation failed! Try again...\n");
-		exit(ERROR_CODE);
-	}
-	if (NULL == (student_dir = (char *)malloc(strlen(directory) + ID_LENGTH + strlen(DIRECTORY_FOR_SON) + 2)))  // student Ids directory name memory allocation
-	{
-		printf("Memory Allocation failed! Try again...");
-		exit(ERROR_CODE);
-	}
+	//if (NULL == (id_filepath = (char *)malloc(strlen(directory) + strlen(ID_FILE_NAME) + 2)))  // student Ids memory allocation
+	//{
+	//	printf("Memory Allocation failed! Try again...\n");
+	//	exit(ERROR_CODE); 
+	//}
+	//if (NULL == (final_filepath = (char *)malloc(strlen(directory) + strlen(FINAL_GRADES_FILE_NAME) + 2)))  // student Ids memory allocation
+	//{
+	//	printf("Memory Allocation failed! Try again...\n");
+	//	exit(ERROR_CODE);
+	//}
+	//if (NULL == (student_dir = (char *)malloc(strlen(directory) + ID_LENGTH + strlen(DIRECTORY_FOR_SON) + 2)))  // student Ids directory name memory allocation
+	//{
+	//	printf("Memory Allocation failed! Try again...");
+	//	exit(ERROR_CODE);
+	//}
+
+	/* merge strings */
 	MergeStrings(id_filepath, directory, ID_FILE_NAME); // concat dir name and studentIds.txt
 	MergeStrings(final_filepath, directory, FINAL_GRADES_FILE_NAME);
+
+	/* call process */
 	ReadIdsAndCallProcess(id_number, directory, student_dir, id_filepath, final_filepath);
+
+	/* free memory allocated in this scope */
 	free(id_filepath);
 	free(final_filepath);
 	free(student_dir);
@@ -82,6 +100,7 @@ void MergeStringsForStudentFinalGrade(char *target, char *first, char *second, c
 	strcat(target, forth);
 }
 
+
 void WriteFinalGradeToFile(char *id_num, char *filename, char *student_path)
 {
 	FILE *fp_students, *fp_final;
@@ -94,7 +113,7 @@ void WriteFinalGradeToFile(char *id_num, char *filename, char *student_path)
 	MergeStringsForStudentFinalGrade(student_file, student_path, FINAL_STUDENT_FILE_NAME, id_num, TXT);
 
 	//files handling
-	if (NULL == (fp_students = fopen(filename, "r+"))) // final_grades.txt
+	if (NULL == (fp_students = fopen(filename, "a"))) // final_grades.txt
 	{
 		printf("File ERROR\n");
 		exit(ERROR_CODE);
