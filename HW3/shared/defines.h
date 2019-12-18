9828fe3940b2793e52d4215fc4ff7368536bab39
roy-pstr <49519972+roy-pstr@users.noreply.github.com>
2019-12-18 12:52:46 +0200

    HW3 first ver

---
+++ HW3/shared/defines.h
@@ -0,0 +1,14 @@
+#ifndef _DEFINES_H
+#define _DEFINES_H
+
+#define ERROR_CODE ((int)(-1))
+#define SUCCESS_CODE ((int)(0))
+#define MAX_NAME_LEN 20
+#define MAX_ROOMS 5
+#define MAX_GUESTS 15
+#define ROOMS_TXT_FILE "rooms.txt"
+#define NAMES_TXT_FILE "names.txt"
+#define LOG_FILE "roomLog.txt"
+//#define INPUT_FOLDER "C:\Users\roypa\OneDrive\Documents\GitHub\system_programming\HW3\inputs\"
+
+#endif
